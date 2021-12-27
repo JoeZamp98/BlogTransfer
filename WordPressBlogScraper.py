5e0e0e3f34baa937cd0915d85c41555ca9e140c4
Joe Zampitella <joezamp98@gmail.com>
2021-12-27 15:24:11 -0500

    Added code to scrape paragraph divs specifically from the main body of an article.

--- WordPressBlogScraper.py
+++ WordPressBlogScraper.py
@@ -5,8 +5,11 @@ import csv
 archivesOne = requests.get('https://josephzampitella.com/author/joezampitella/').text
 archivesTwo = requests.get('https://josephzampitella.com/author/joezampitella/page/2').text
 
+testArticleOne = requests.get('https://josephzampitella.com/2020/12/16/winter-storm-expected-thursday-12-17/').text
+
 soupOne = BeautifulSoup(archivesOne, 'lxml')
 soupTwo = BeautifulSoup(archivesTwo, 'lxml')
+soupThree = BeautifulSoup(testArticleOne, 'lxml')
 
 csv_file = open('wordpress_scraped.csv', 'w')
 
@@ -20,3 +23,16 @@ for article in soupOne.find_all('article'):
 for article in soupTwo.find_all('article'):
     headline = article.h2.a.text
     print(headline)
+
+# for paragraph in soupThree.find_all('article'):
+#     content = paragraph.p.a.text
+#     print(content)
+
+section = soupThree.findAll('div', attrs={"class":"entry-content"})
+
+htmlParse = BeautifulSoup(testArticleOne, 'html.parser')
+
+div = soupThree.find(class_='entry-content')
+
+for article in div.find_all('p'):
+     print(article.get_text())
