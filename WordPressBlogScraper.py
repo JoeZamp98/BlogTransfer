4068c326b825dae7793abdce392082b76372cd5b
Joe Zampitella <joezamp98@gmail.com>
2021-12-27 17:51:58 -0500

    Partially solved issue with each paragraph acting as its own item in a list.  Working to concatenate all paragraphs within an article into a single list item.

--- WordPressBlogScraper.py
+++ WordPressBlogScraper.py
@@ -2,16 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+from requests.exceptions import ConnectTimeout
+
 archivesOne = requests.get('https://josephzampitella.com/author/joezampitella/').text
 archivesTwo = requests.get('https://josephzampitella.com/author/joezampitella/page/2').text
 
-testArticleOne = requests.get('https://josephzampitella.com/2020/12/16/winter-storm-expected-thursday-12-17/').text
-
 soupOne = BeautifulSoup(archivesOne, 'lxml')
 soupTwo = BeautifulSoup(archivesTwo, 'lxml')
-soupThree = BeautifulSoup(testArticleOne, 'lxml')
 
-#Opens 
+#Opens CSV file
 
 csv_file = open('wordpress_scraped.csv', 'w')
 
@@ -32,10 +31,16 @@ for article in soupTwo.find_all('article'):
     #print(headline)
     articleTitles.append(headline)
 
+del(articleTitles[3]) #this article was missing an HREF link; will investigate
+
 #Retrieves body text from individual posts
 
 hrefList = []
 contentList =[]
+
+for a in soupOne.find_all('a', href=True, class_='more-link'):
+    if a.text:
+        hrefList.append(a['href'])
 
 for a in soupTwo.find_all('a', href=True, class_='more-link'):
      if a.text:
@@ -48,10 +53,53 @@ for link in hrefList:
     
     div = postSoup.find(class_='entry-content')
 
+    contentContainer = str()
+
     for section in div.find_all('p'):
 
         contentText = section.get_text()
 
-        contentList.append(contentText)
+        #print(contentText)
+
+        finalContainer = contentContainer + contentText
+
+    contentList.append(finalContainer)
+
+    del(finalContainer)
+        
+counter = 0
+
+while counter <= 15: 
+
+    writeTitle = articleTitles[counter]
+    writeLink = hrefList[counter]
+    writeContent = contentList[counter]
+
+    csv_writer.writerow([writeTitle, writeLink, writeContent])
+
+    counter += 1
 
 csv_file.close()
+
+print(contentText)
+
+##Test append
+
+oneLink = hrefList[3]
+
+print(oneLink)
+
+postLink = requests.get('https://josephzampitella.com/2020/10/16/more-beneficial-rain-on-the-way/').text
+postSoup = BeautifulSoup(postLink, 'lxml')
+
+div = postSoup.find(class_='entry-content')
+
+
+
+for section in div.find_all('p'):
+
+    contentText = section.get_text()
+
+    print(contentText)
+
+print('\n', contentText)
