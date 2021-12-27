da72e0bf6aeee2e15ba078261777c752b76174ff
Joe Zampitella <joezamp98@gmail.com>
2021-12-27 14:36:16 -0500

    Added support for scraping multiple archive pages.

--- WordPressBlogScraper.py
+++ WordPressBlogScraper.py
@@ -2,12 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-source = requests.get('https://josephzampitella.com/author/joezampitella/').text
+archivesOne = requests.get('https://josephzampitella.com/author/joezampitella/').text
+archivesTwo = requests.get('https://josephzampitella.com/author/joezampitella/page/2').text
 
-soup = BeautifulSoup(source, 'lxml')
+soupOne = BeautifulSoup(archivesOne, 'lxml')
+soupTwo = BeautifulSoup(archivesTwo, 'lxml')
 
 csv_file = open('wordpress_scraped.csv', 'w')
 
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['headline', 'date', 'full_text'])
 
+for article in soupOne.find_all('article'):
+    headline = article.h2.a.text
+    print(headline)
+
+for article in soupTwo.find_all('article'):
+    headline = article.h2.a.text
+    print(headline)
